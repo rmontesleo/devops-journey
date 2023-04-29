6bccd8f2b54d9fc577ba6b0bbdc2ca55b2b9afac
Leopoldo Ramirez <rmontesleo@gmail.com>
2023-04-28 21:05:00 -0600

    add the sample of Amin Espinoza

---
+++ terraform/tutorials/content_creators/amin_espinoza/terraform.tfvars
@@ -0,0 +1,6 @@
+
+resource_group_name = "virtualMachineTerraform"
+location = "westus"
+admin_username = "azureuser"
+vmcount = 3
+admin_password = "Super Secret"
---
+++ terraform/tutorials/content_creators/amin_espinoza/variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+    type = string
+    description = "(optional) describe your variable"
+}
+
+variable "location" {
+    type = string
+    description = "(optional) describe your variable"
+}
+
+variable "vmcount" {
+    type = number
+    description = "(optional) describe your variable"
+}
+
+variable "admin_username" {
+    type = string
+    description = "(optional) describe your variable"
+}
+
+variable "admin_password" {
+    type = string
+    description = "(optional) describe your variable"
+}
